4a73bc1b8e495cd33c7eec2e4a76114213fe0cd1
Samar Negm <negmsamar890@gmail.com>
2021-12-22 00:30:23 +0200

    details

--- js/contact.js
+++ js/contact.js
@@ -2,10 +2,7 @@ var mycart = JSON.parse(localStorage.getItem("cart"));
 var productPadgeCount = document.getElementById("badgeNumber");
 
 productPadgeCount.innerHTML = mycart.length;
-var product = JSON.parse(localStorage.getItem("product"));
-document.getElementById("productName").innerHTML = product.name;
-document.getElementById("ProductImage").setAttribute("src", product.image);
-document.getElementById("productDescription").innerHTML = product.description;
+
 var cartIcon = document.getElementById("badgecontainer");
 cartIcon.addEventListener("click", function (e) {
     win = window.open("cart.html", '_blank');
--- js/detailes.js
+++ js/detailes.js
@@ -1,6 +1,9 @@
 var mycart = JSON.parse(localStorage.getItem("cart"));
 var productPadgeCount = document.getElementById("badgeNumber");
-
+var product = JSON.parse(localStorage.getItem("product"));
+document.getElementById("productName").innerHTML = product.name;
+document.getElementById("ProductImage").setAttribute("src", product.image);
+document.getElementById("productDescription").innerHTML = product.description;
 productPadgeCount.innerHTML = mycart.length;
 var cartIcon = document.getElementById("badgecontainer");
 cartIcon.addEventListener("click", function (e) {
